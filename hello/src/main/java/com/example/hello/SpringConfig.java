20c47fd8ce07575c5264f1d49ca62427d97b155a
Hminchae <hminchae@gmail.com>
2025-02-10 23:24:40 +0900

    Add : 자바 코드로 스프링 빈 등록하기

---
+++ hello/src/main/java/com/example/hello/SpringConfig.java
@@ -0,0 +1,24 @@
+package com.example.hello;
+
+import com.example.hello.repository.MemberRepository;
+import com.example.hello.repository.MemoryMemberRepository;
+import com.example.hello.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
+
+// 자바 코드로 직접 스프링 빈 등록하는 방법
+//
